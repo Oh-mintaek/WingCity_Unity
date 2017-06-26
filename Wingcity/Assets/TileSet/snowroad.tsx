d85587460e0b3a662e956febf15db3157632fe58
Yuria <mint_h@naver.com>
2017-06-26 20:20:49 +0900

    new char_base added, new char_animation added

---
+++ Wingcity/Assets/TileSet/snowroad.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="snowroad" tilewidth="32" tileheight="32" spacing="1" tilecount="16" columns="4">
+ <image source="frame.png" width="131" height="131"/>
+</tileset>

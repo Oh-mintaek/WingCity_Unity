07e4418c7e775fd9474cddd568d364cf3a2a3152
Yuria <mint_h@naver.com>
2017-06-29 01:50:04 +0900

    Map added

---
+++ Tiled/Tiled_Tileset/block_roof.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="block_roof" tilewidth="64" tileheight="64" tilecount="24" columns="8">
+ <image source="block_roof.png" width="512" height="192"/>
+</tileset>
---
+++ Tiled/Tiled_Tileset/fence_full.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="fence_full" tilewidth="64" tileheight="64" tilecount="16" columns="4">
+ <image source="fence_full.png" width="256" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="27.25" width="63.5" height="35.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="5.5" width="63.25" height="34.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="27.75" width="36" height="34.5"/>
+   <object id="2" x="27.5" y="0.75" width="9" height="26.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="27.25" y="1" width="9.25" height="62.5"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="1" x="27.5" y="5.25" width="36" height="35.25"/>
+   <object id="2" x="27.5" y="41.25" width="9" height="22.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="3" x="0.25" y="5.75" width="36.25" height="34.75"/>
+   <object id="4" x="27.25" y="40.5" width="9.25" height="23"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="27.25" y="0.25" width="9.25" height="62.25"/>
+   <object id="2" x="36.75" y="27" width="26.75" height="35.25"/>
+  </objectgroup>
+ </tile>
+</tileset>

0a5a1a2a2503f395f3c587bedaac2e5d73d1c4cc
Yuria <mint_h@naver.com>
2017-07-13 03:26:33 +0900

    Please enter the commit message for your changes. Lines starting

---
+++ Wingcity/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuManager : MonoBehaviour {
+
+    public GameObject[] mBox;
+    public Text[] dText;
+    public int menuNumber;
+    public bool[] menuActive;
+    public int menuOnOff;
+
+    protected bool paused;
+
+
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i <= menuNumber; i++)
+        {
+            mBox[i].SetActive(false);           
+
+        }
+        menuOnOff = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(menuOnOff !=2 && Input.GetKeyDown(KeyCode.Escape))
+        {
+            for (int i = 0; i <= menuNumber; i++)
+            {                
+                mBox[i].SetActive(true);                
+            }
+
+            menuOnOff++;
+            Time.timeScale = 0;
+        }
+
+        if (menuOnOff == 2)
+        {
+            for (int i = 0; i <= menuNumber; i++)
+            {
+                mBox[i].SetActive(false);                
+            }
+            menuOnOff = 0;
+            Time.timeScale = 1;
+        }
+
+	}
+}
